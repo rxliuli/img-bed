290f862fff214c618ed25d74b2ab6b8283f50192
rxliuli <rxliuli@gmail.com>
2020-05-19 13:26:38 +0800

    build temp

--- build.js
+++ build.js
@@ -2,14 +2,24 @@ const path = require('path')
 const fs = require('fs')
 
 const prefix = 'https://img.rxliuli.com/'
-const list = fs.readdirSync(path.resolve(__dirname, '.'))
 
-fs.writeFileSync(
-  path.resolve(__dirname, 'DataList.json'),
-  JSON.stringify(
-    list.filter(name => name.endsWith('.png')).map(name => prefix + name),
-  ),
-  {
-    encoding: 'utf8',
-  },
-)
+try {
+  console.log('开始读取图片列表')
+  const list = fs.readdirSync(path.resolve(__dirname, '.'))
+
+  console.log('开始进行转换图片列表')
+  const urlList = list
+    .filter(name => name.endsWith('.png'))
+    .map(name => prefix + name)
+
+  console.log('开始写入配置文件')
+  fs.writeFileSync(
+    path.resolve(__dirname, 'DataList.json'),
+    JSON.stringify(urlList),
+    {
+      encoding: 'utf8',
+    },
+  )
+} catch (e) {
+  console.log('构建发生了错误: ', e)
+}
