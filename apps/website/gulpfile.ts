a2655bb039fd1aaa0c76cb550b7d3dfad7bf55af
rxliuli <rxliuli@gmail.com>
2021-06-02 01:00:51 +0800

    feat(website): 浏览图床图片基本实现

---
+++ apps/website/gulpfile.ts
@@ -0,0 +1,49 @@
+import * as path from 'path'
+import { readdir, writeJSON } from 'fs-extra'
+import { AsyncArray } from '@liuli-util/async'
+import imageSize from 'image-size'
+
+const suffix = new Set([
+  'BMP',
+  'CUR',
+  'DDS',
+  'GIF',
+  'ICNS',
+  'ICO',
+  'JPEG',
+  'KTX',
+  'PNG',
+  'PNM',
+  'PAM',
+  'PBM',
+  'PFM',
+  'PGM',
+  'PPM',
+  'PSD',
+  'SVG',
+  'TIFF',
+  'WebP',
+])
+
+/**
+ * 生成扫描到的图片元数据
+ */
+export async function generate() {
+  const dir = path.resolve(__dirname, '../../images')
+  const names = await readdir(path.resolve(dir))
+  const list = await AsyncArray.map(
+    names.filter((name) =>
+      suffix.has(path.extname(name).substring(1).toUpperCase()),
+    ),
+    async (name) => {
+      const size = imageSize(path.resolve(dir, name))
+      return {
+        width: size.width,
+        height: size.height,
+        src: `https://img.rxliuli.com/${name}`,
+        name,
+      }
+    },
+  )
+  await writeJSON(path.resolve(__dirname, 'src/photos.json'), list)
+}
--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import photos from './photos.json'
+import Gallery from 'react-photo-gallery'
 
 function App() {
-  return <div>App</div>
+  const openLightbox = useCallback((event, { photo, index }) => {
+    window.open(photo.src, '_blank')
+  }, [])
+
+  return (
+    <div>
+      <Gallery photos={photos} margin={4} onClick={openLightbox} />
+    </div>
+  )
 }
 
 export default App
